a0b778882d31c4d2092514707625986d44d34c5d
MaxandreOgeret <MaxandreOgeret@users.noreply.github.com>
2021-04-13 20:06:34 +0300

    Corrected bug where REFLECTIVITY was put in place of RANGE in the Point.

--- ros2_ouster/include/ros2_ouster/conversions.hpp
+++ ros2_ouster/include/ros2_ouster/conversions.hpp
@@ -265,7 +265,7 @@ static void toCloud(
         static_cast<uint16_t>(pix(ouster::LidarScan::REFLECTIVITY)),
         static_cast<uint8_t>(u),
         static_cast<uint16_t>(pix(ouster::LidarScan::AMBIENT)),
-        static_cast<uint32_t>(pix(ouster::LidarScan::REFLECTIVITY))};
+        static_cast<uint32_t>(pix(ouster::LidarScan::RANGE))};
     }
   }
 }
