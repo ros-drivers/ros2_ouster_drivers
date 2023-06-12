0a79706fdff4917bdf4f9d9f88ae263f3f27c95d
Imaniac230 <44968160+Imaniac230@users.noreply.github.com>
2023-06-12 19:25:21 +0200

    fix: Compile this on humble and start testing the ouster's compatibility branch.

--- ros2_ouster/include/ros2_ouster/conversions.hpp
+++ ros2_ouster/include/ros2_ouster/conversions.hpp
@@ -27,10 +27,11 @@
 #include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <tf2/LinearMath/Transform.h>
-#include <tf2_eigen/tf2_eigen.h>
 #ifdef TF2_CPP_HEADERS
+  #include <tf2_eigen/tf2_eigen.hpp>
   #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #else
+  #include <tf2_eigen/tf2_eigen.h>
   #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #endif
 
